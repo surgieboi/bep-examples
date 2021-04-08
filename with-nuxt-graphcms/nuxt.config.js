6beeac24b8f513b895a4fb8e26e54143e1285a19
Adeniyi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2021-04-08 20:41:58 +0100

    use ssg

--- with-nuxt-graphcms/nuxt.config.js
+++ with-nuxt-graphcms/nuxt.config.js
@@ -1,13 +1,10 @@
 export default {
-	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-	ssr: false,
-
 	// Target: https://go.nuxtjs.dev/config-target
 	target: "static",
 
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: "test-nuxt",
+		title: "bep-nuxt-graphcms",
 		htmlAttrs: {
 			lang: "en",
 		},
@@ -16,7 +13,6 @@ export default {
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
 			{ hid: "description", name: "description", content: "" },
 		],
-		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 		script: [
 			{
 				hid: "Dola",
